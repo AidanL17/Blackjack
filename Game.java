55f06ed26df1f3e372c6f504e8d90156da319383
Aidan Lavelle <360247@beaverton.k12.or.us>
2016-06-01 10:34:47 -0700

    Add a bunch of frame/layout stuff

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -5,22 +7,68 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class Game implements ActionListener {
 	
 	JFrame frame = new JFrame("Blackjack");
-	JButton dealTest = new JButton("Pick a card, any card");
+	JButton hit = new JButton("Hit");
 	boolean cards[][] = new boolean[13][4];
+	int bank = 100;
+	int bet = 0;
+	Container west = new Container();
+	Container east = new Container();
+	Container betChange = new Container();
+	JButton doubleDown = new JButton("Double Down");
+	JButton surrender = new JButton("Surrender");
+	JButton stand = new JButton("Stand");
+	JButton allIn = new JButton("BET IT ALL");
+	JButton plusTen = new JButton("+ $10");
+	JButton plusFive = new JButton("+ $5");
+	JButton plusOne = new JButton("+ $1");
+	JButton minusTen = new JButton("- $10");
+	JButton minusFive = new JButton("- $5");
+	JButton minusOne = new JButton("- $1");
+	JTextArea fund = new JTextArea("Your kid's college fund: $" + bank);
+	JTextArea currentBet = new JTextArea("Your bet: $" + bet);
 
 	public static void main(String[] args) {
 		new Game();
 	}
 	
 	public Game() {
-		frame.setSize(400, 400);
-		frame.setLayout(new GridLayout(1,1));
-		frame.add(dealTest);
-		dealTest.addActionListener(this);
+		frame.setSize(800, 400);
+		frame.setLayout(new BorderLayout());
+		west.setLayout(new GridLayout(4,1));
+		east.setLayout(new GridLayout(4,1));
+		betChange.setLayout(new GridLayout(3,2));
+		betChange.add(plusTen);
+		betChange.add(minusTen);
+		betChange.add(plusFive);
+		betChange.add(minusFive);
+		betChange.add(plusOne);
+		betChange.add(minusOne);
+		east.add(fund);
+		east.add(currentBet);
+		east.add(betChange);
+		east.add(allIn);
+		west.add(hit);
+		west.add(doubleDown);
+		west.add(surrender);
+		west.add(stand);
+		hit.addActionListener(this);
+		doubleDown.addActionListener(this);
+		surrender.addActionListener(this);
+		stand.addActionListener(this);
+		plusTen.addActionListener(this);
+		minusTen.addActionListener(this);
+		plusFive.addActionListener(this);
+		minusFive.addActionListener(this);
+		plusOne.addActionListener(this);
+		minusOne.addActionListener(this);
+		allIn.addActionListener(this);
+		frame.add(west, BorderLayout.WEST);
+		frame.add(east, BorderLayout.EAST);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -45,7 +93,7 @@ public class Game implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
-		if (event.getSource().equals(dealTest)) {
+		if (event.getSource().equals(hit)) {
 			Deal();
 		}
 	}
